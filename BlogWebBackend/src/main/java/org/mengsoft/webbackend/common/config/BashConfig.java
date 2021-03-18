8cc970714e5a10bda98abe3e1ff8acbfce64d7c7
Lingjun Meng <575628304@qq.com>
2021-03-19 07:31:17 +0800

    Syn March

---
+++ BlogWebBackend/src/main/java/org/mengsoft/webbackend/common/config/BashConfig.java
@@ -0,0 +1,7 @@
+package org.mengsoft.webbackend.common.config;
+
+import org.springframework.beans.factory.annotation.Value;
+
+public class BashConfig {
+  //@Value("${spring.swagger2.enabled}")
+}
